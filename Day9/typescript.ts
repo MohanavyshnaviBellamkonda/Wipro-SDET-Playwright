283485878f3665472ac543d2e9beaace607511d9
MohanavyshnaviBellamkonda <mohanavyshnavi2003@gmail.com>
2026-05-11 12:53:08 +0530

    updated

--- Day9/typescript.ts
+++ Day9/typescript.ts
@@ -1,2 +1,15 @@
 let name = "vyshu";
 console.log(name);
+
+let name1 :string = "phani";
+name1 = 10; //give the error that name1 is string 
+let output: number | string
+output = 10;
+output = "phani";
+console.log(output);
+
+function add(a:number, b:number):number
+{
+    return  (a+b);
+}
+console.log(add(5,10));
