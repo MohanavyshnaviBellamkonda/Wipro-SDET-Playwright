68fa622456beed316606446d8d61d44d32a271be
MohanavyshnaviBellamkonda <mohanavyshnavi2003@gmail.com>
2026-05-18 16:55:06 +0530

    tests with annotations

---
+++ Day15/tests/example.spec.js
@@ -0,0 +1,47 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+/*test('amazon razor products check', async ({ page }) => {
+  await page.goto('https://www.amazon.com/');
+
+  const txtBox = page.locator('#twotabsearchtextbox');
+  await txtBox.fill("gaming");
+  const subBtn = page.locator('#nav-search-submit-button');
+  await subBtn.click();
+
+
+
+//test.skip
+
+test.skip('ok', async ({ page }) => {
+  //hello
+});
+
+//test.fail
+
+test.step('check if 2 is equal to 2', ()  => {
+  test.fail();
+  expect(2).toBe(2);
+});
+
+test.step('check if 5 is equal to 5', ()  => {
+  test.fail();
+  expect(5).toBe(5);
+});
+
+});
+
+
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.amazon.com/');
+  await page.getByRole('searchbox', { name: 'Search Amazon' }).click();
+  await page.getByRole('searchbox', { name: 'Search Amazon' }).fill('gaming');
+  await page.goto('https://www.amazon.com/s?k=gaming&crid=1L6I27L40C2RQ&sprefix=gaming%2Caps%2C393&ref=nb_sb_noss_1');
+  await page.locator('.s-widget-container.s-spacing-small.s-widget-container-height-small.celwidget.slot\\=MAIN.template\\=SEARCH_RESULTS.widgetId\\=search-results_37 > span > .puis-card-container > .a-section.a-spacing-base > .s-product-image-container > .rush-component > .a-link-normal').click();
+  await page.goto('https://www.amazon.com/Razer-Viper-Wireless-Esports-Gaming/dp/B0GMLBSSTD/ref=sr_1_37?crid=1L6I27L40C2RQ&dib=eyJ2IjoiMSJ9.hLLcledmCAw7Gph_I_m6RN8RICiKZbrHq6l3liJ4CeKRwzQd_SEMqhqUZNX1qtjLhHb57C6yrK8oIiCdnXHW-3GWV-laH6pHVC5rqKiH9HVJWQkH6PQiaSrhVIO_QFkuSYy4-D6a4mDaSGBON0YHQJfB8e2sG8paCvPUyn8p-OSVfYzKhBq4aHbRjmy_j3nD-zUXdRZdFyuCYqy0REhft4E1fYYNJoM7JbwJIoANx8cuhRARUQTiCLMvccQpz3yGY_HOimadhTbdwtafhNAnqgxRlp5ASWdeOBYkTIbbWUk.j8FjmnYUyVcnDqHC9fa3BLwEq_7MkckqUHQd28MtfcQ&dib_tag=se&keywords=gaming&qid=1779083528&sprefix=gaming%2Caps%2C393&sr=8-37&th=1');
+});
+
+*/
